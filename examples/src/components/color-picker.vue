98c62e9e998cfa153e593e404eb5bb3cd43a4a62
kbz85 <248997917@qq.com>
2023-05-25 15:25:00 +0800

    1

--- examples/src/components/color-picker.vue
+++ examples/src/components/color-picker.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ColorPicker v-model:pure-color="selector"  ></ColorPicker>
+        <ColorPicker v-model:pure-color="selector"></ColorPicker>
         <!-- < v-model:value="selector"></> @pure-color-change="changevalue"-->
     </div>
 </template>
@@ -9,12 +9,29 @@ import { ref, watch } from "vue";
 import { ColorPicker } from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
 const selector = ref('rgba(24,144,255, 1)')
-watch(selector, (oldV:string, newV:string) => {
-    console.log(oldV, newV);
+watch(selector, (newV:string, oldV:string) => {
+    // console.log(oldV, newV);
     const styles = document.getElementsByTagName('style');
     const root = Array.from(styles).find(item => {
         return item.getAttribute('theme')
     })
+    let oldStrTailwind = ''
+    if (oldV.includes('rgba')) {
+        oldStrTailwind = oldV.substring(oldV.indexOf('(') + 1, oldV.indexOf('1)')).replaceAll(',', ' ').trim();
+    } else {
+        oldStrTailwind = oldV.substring(oldV.indexOf('(') + 1, oldV.indexOf(')')).replaceAll(',', ' ').trim();
+    }
+    // const oldStrTailwind = oldV.substring(oldV.indexOf('(') + 1, oldV.indexOf(')')).replaceAll(',', ' ');
+    const strTailwind = newV.substring(newV.indexOf('(') + 1, newV.indexOf(')')).replaceAll(',', ' ').trim();
+    console.log(oldStrTailwind);
+    
+    if (root && root.textContent) {
+        root.textContent = root.textContent.replaceAll(oldV, newV).replaceAll(oldStrTailwind, strTailwind)
+        console.log(root.textContent);
+        
+    }
+    console.log(oldStrTailwind, strTailwind);
+    
     // console.log(root?.textContent);
 })
 // const changevalue = (oldV:string, value: string) => {
@@ -32,7 +49,6 @@ watch(selector, (oldV:string, newV:string) => {
 //         extractedString = extractedString.replaceAll(',', '')
 //         root.style.setProperty('--primary-color-tailwindcss', extractedString);
 //     }
-
 // }
 </script>
 <style lang='less'></style>
